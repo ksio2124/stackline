d357664f990b6c3fd029f639a3c5c39b9ca01ab3
ksio2124 <ksio2124@protonmail.ch>
2024-05-20 23:57:12 -0700

    React Table: Add data + library

--- my-ts-app/src/App.tsx
+++ my-ts-app/src/App.tsx
@@ -1,12 +1,78 @@
 import React from 'react';
 import logo from './logo.svg';
+import data from './static/stackline_frontend_assessment_data_2021.json'
 import './App.css';
+import { useReactTable } from'@tanstack/react-table';
+
+function SaleTable({columns, saleData}) {
+  const tableInstance = useReactTable({ columns, saleData });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
 function App() {
+  const saleData = React.useMemo(
+    () => data[0].sales, [data]
+  )
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Week Ending',
+        accessor: 'weekEnding', 
+ 
+      },
+      {
+        Header: 'Retail Sales',
+        accessor: 'retailSales',
+      },
+      {
+        Header: 'Wholesale Sales',
+        accessor: 'wholesaleSales', 
+ 
+      },
+      {
+        Header: 'Units Sold',
+        accessor: 'unitsSold',
+      },
+      {
+        Header: 'Retailer Margin',
+        accessor: 'retailerMargin',
+      },
+    ],
+    []
+  )
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='flex flex-col h-screen'>
+      <div className='h-8 bg-blue-500 grow-0'>
+      </div>
+      <div className='bg-slate-500 flex grow'>
+        <div className='m-2 grow-0 rounded-lg bg-white p-4'>
+          <img src={data[0].image} />
+
+        </div>
+        <div className='grow flex flex-col'>
+          <div className='m-2 bg-white rounded-lg grow'></div>
+          <div className='m-2 bg-white rounded-lg grow'>
+            <SaleTable columns={columns} saleData={saleData} />
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
